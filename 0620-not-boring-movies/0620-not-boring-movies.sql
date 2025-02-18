d6e4dae0f8b3f1d4af932f35001f1ac4698f8bce
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-02-18 16:37:31 +0900

    Time: 410 ms (55.8%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,11 +1,6 @@
 # Write your MySQL query statement below
-SELECT C1.id, C1.movie, C1.description, C1.rating
-FROM Cinema C1
-INNER JOIN
-(
-    SELECT id, MOD(id,2) <> 0 odd
-    FROM Cinema
-) C2
-ON C1.id = C2.id
-WHERE description <> "boring" AND odd = 1
+SELECT id, movie, description, rating
+FROM Cinema 
+WHERE description <> "boring" 
+AND id % 2 = 1
 ORDER BY rating  DESC
