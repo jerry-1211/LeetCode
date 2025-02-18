5d492c1f98b6be357d2fba705f02652b58101c03
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-02-18 14:41:32 +0900

    Time: 493 ms (82.28%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -3,10 +3,10 @@ SELECT E1.name
     FROM Employee E1
     INNER JOIN 
     (
-        SELECT *, COUNT(*) COUNT
-        FROM Employee 
-        WHERE managerId IS NOT NULL
-        GROUP BY managerId 
+        SELECT managerId 
+            FROM Employee 
+            WHERE managerId IS NOT NULL
+            GROUP BY managerId 
+            HAVING COUNT(*) >= 5
     ) E2
     ON E1.id  = E2.managerId 
-    WHERE E2.COUNT >= 5
